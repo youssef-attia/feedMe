1ca8e8d91c1e9ba28fd2d78a97c63fe6ee80d114
YAttia <attia_youssef@hotmail.com>
2020-06-30 11:44:30 -0400

    Updates

---
+++ precache-manifest.5bc33a5faadef9b8a73276b3e7cdb822.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295e8e006d746dcf2d999a7e94481d57",
+    "url": "/feedMe/index.html"
+  },
+  {
+    "revision": "661815e1a5e8c85f7f1f",
+    "url": "/feedMe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0fb26bfa109f858c3e23",
+    "url": "/feedMe/static/css/main.02dae044.chunk.css"
+  },
+  {
+    "revision": "661815e1a5e8c85f7f1f",
+    "url": "/feedMe/static/js/2.95d89231.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/feedMe/static/js/2.95d89231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb26bfa109f858c3e23",
+    "url": "/feedMe/static/js/main.2a9cf005.chunk.js"
+  },
+  {
+    "revision": "a0642763464b1a545da0",
+    "url": "/feedMe/static/js/runtime-main.4c65668f.js"
+  },
+  {
+    "revision": "96f4d1fc976a4aed79cf20bdb10b2506",
+    "url": "/feedMe/static/media/spoon.96f4d1fc.png"
+  }
+]);
